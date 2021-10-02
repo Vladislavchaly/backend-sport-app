45b784f38f7e1d0f7cd75c980c7bdfe83e45df94
chaly <Chaly95@gmail.com>
2021-10-02 12:53:44 +0300

    add singIn & singUp controllers and add singIn & signUp Validators

---
+++ app/Controllers/Http/Api/v1/Auth/SignIn/SignInController.ts
@@ -0,0 +1,21 @@
+'use strict'
+import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import SignInvalidator from "App/Validators/Api/v1/Auth/SignInValidator";
+import User from "App/Models/User";
+import {OpaqueTokenContract} from "@ioc:Adonis/Addons/Auth";
+
+export default class SignInController {
+
+  public async index({auth, request}: HttpContextContract): Promise<ReturnType<OpaqueTokenContract<User>['toJSON']>> {
+
+    await request.validate(SignInvalidator)
+
+    const email: string = request.input('email');
+    const password: string = request.input('password');
+
+    return await auth.use('api').attempt(email, password, {
+      name: request.header('user-agent')
+    });
+
+  }
+}
